85f1b6e9bfa1c9d0d302517689827a4bf23c64b2
Wilson <wilsonhorrell@git.training>
2020-08-06 19:15:46 -0500

    placed routes at end of middleware

--- server.js
+++ server.js
@@ -1,65 +1,68 @@
+const express = require("express");
+const app = express();
 const db = require('./db/database');
 const inputCheck = require("./utils/inputCheck");
 
-const express = require("express");
+
 const PORT = process.env.PORT || 3001;
-const app = express();
+
 const apiRoutes = require('./routes/apiRoutes');
-app.use('/api', apiRoutes);
+
 
 //Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use('/api', apiRoutes);
 
- //Create a Candidate
- const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
- VALUES (?,?,?,?)`;
-const params = [1, "Ronald", "Firbank", 1];
-//ES5 function, not arrow function, to use this
-db.run(sql, params, function (err, result) {
-if (err) {
-console.log(err);
-}
-console.log(result, this.lastID);
-});
+//  //Create a Candidate
+//  const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//  VALUES (?,?,?,?)`;
+// const params = [1, "Ronald", "Firbank", 1];
+// //ES5 function, not arrow function, to use this
+// db.run(sql, params, function (err, result) {
+// if (err) {
+// console.log(err);
+// }
+// console.log(result, this.lastID);
+// });
 
 // PUT Request Api Route for Updating information
-app.put('/api/candidate/:id', (req, res) => {
-  const errors = inputCheck(req.body, 'party_id');
-  
-  if (errors) {
-    res.status(400).json({ error: errors });
-    return;
-  }
-  const sql = `UPDATE candidates SET party_id = ?
-                WHERE id = ?`;
-  const params = [req.body.party_id, req.params.id];
+// app.put('/api/candidate/:id', (req, res) => {
+//   const errors = inputCheck(req.body, 'party_id');
 
-  db.run(sql, params, function(err, result) {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
+//   if (errors) {
+//     res.status(400).json({ error: errors });
+//     return;
+//   }
+//   const sql = `UPDATE candidates SET party_id = ?
+//                 WHERE id = ?`;
+//   const params = [req.body.party_id, req.params.id];
 
-    res.json({
-      message: 'success',
-      data: req.body,
-      changes: this.changes
-    });
-  });
-});
+//   db.run(sql, params, function(err, result) {
+//     if (err) {
+//       res.status(400).json({ error: err.message });
+//       return;
+//     }
+
+//     res.json({
+//       message: 'success',
+//       data: req.body,
+//       changes: this.changes
+//     });
+//   });
+// });
 
 
 
 // Default response for any other request (Not Found) Catch all
 app.use((req, res) => {
-  res.status(404).end();
+    res.status(404).end();
 });
 
 //Start Server after DB connection
 //Connection function will start Express.js server on port 3001
 db.on("open", () => {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
 });
